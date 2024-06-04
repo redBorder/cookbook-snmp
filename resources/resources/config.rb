ad5289a726888194f131c45c9253183f4bdc5474
Miguel Negron <manegron@redborder.com>
2024-06-04 23:20:22 +0100

    lint resources

--- resources/resources/config.rb
+++ resources/resources/config.rb
@@ -1,14 +1,12 @@
-# Cookbook Name:: snmp
-#
+# Cookbook:: snmp
 # Resource:: config
-#
 
 actions :add, :remove
 default_action :add
 
-attribute :community, :kind_of => String, :default => "redborder"
-attribute :cdomain, :kind_of => String, :default => "redborder.cluster"
-attribute :hostname, :kind_of => String, :default => "localhost"
-attribute :snmp_username, :kind_of => String, :default => "redborder"
-attribute :snmp_pass, :kind_of => String, :default => "redborderP@ssw0rd"
-attribute :config_dir, :kind_of => String, :default => "/etc/snmp"
+attribute :community, kind_of: String, default: 'redborder'
+attribute :cdomain, kind_of: String, default: 'redborder.cluster'
+attribute :hostname, kind_of: String, default: 'localhost'
+attribute :snmp_username, kind_of: String, default: 'redborder'
+attribute :snmp_pass, kind_of: String, default: 'redborderP@ssw0rd'
+attribute :config_dir, kind_of: String, default: '/etc/snmp'
