33d0f7b52fa825a53f3d7bc4b48f282974c61bc2
Miguel Negron <manegron@redborder.com>
2024-06-04 23:20:08 +0100

    lint providers

--- resources/providers/config.rb
+++ resources/providers/config.rb
@@ -1,8 +1,6 @@
-#
-# Cookbook Name:: snmp
+# Cookbook:: snmp
 # Provider::config
-#
-# Copyright 2010, Eric G. Wolfe
+# Copyright:: 2010, Eric G. Wolfe
 # Based on Eric G. Wolfe recipe for snmp. Adapted to be a provider and to
 # work on redborder environments by Pablo Nebrera and Alberto Rodriguez
 #
@@ -17,7 +15,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
 action :add do
   begin
@@ -28,61 +25,58 @@ action :add do
     snmp_pass = new_resource.snmp_pass
     config_dir = new_resource.config_dir
 
-    dnf_package "net-snmp" do
+    dnf_package 'net-snmp' do
       action :upgrade
     end
 
     directory config_dir do
-      owner "root"
-      group "root"
-      mode "0755"
+      owner 'root'
+      group 'root'
+      mode '0755'
     end
 
-    template "/etc/sysconfig/snmpd" do
-      source "snmpd.erb"
-      owner "root"
-      group "root"
-      mode 0644
+    template '/etc/sysconfig/snmpd' do
+      source 'snmpd.erb'
+      owner 'root'
+      group 'root'
+      mode '0644'
       retries 2
-      cookbook "snmp"
-      variables(:hostname => hostname)
-      notifies :restart, "service[snmpd]", :delayed
+      cookbook 'snmp'
+      variables(hostname: hostname)
+      notifies :restart, 'service[snmpd]', :delayed
     end
 
     template "#{config_dir}/snmpd.conf" do
-      mode 0644
-      owner "root"
-      group "root"
-      source "snmpd.conf.erb"
+      mode '0644'
+      owner 'root'
+      group 'root'
+      source 'snmpd.conf.erb'
       retries 2
-      cookbook "snmp"
-      variables(:community => community,
-                :cdomain => cdomain,
-                :hostname => hostname,
-                :snmp_pass => snmp_pass, 
-                :snmp_username => snmp_username)                
-      notifies :restart, "service[snmpd]", :delayed
+      cookbook 'snmp'
+      variables(community: community,
+                cdomain: cdomain,
+                hostname: hostname,
+                snmp_pass: snmp_pass,
+                snmp_username: snmp_username)
+      notifies :restart, 'service[snmpd]', :delayed
     end
 
-    service "snmpd" do
+    service 'snmpd' do
       ignore_failure true
-      supports :status => true, :reload => true, :restart => true
+      supports status: true, reload: true, restart: true
       action([:start, :enable])
     end
-
   rescue
     Chef::Log.error(e.message)
-  end  
+  end
 end
 
 action :remove do
   begin
-    service "snmpd" do
+    service 'snmpd' do
       ignore_failure true
-      supports :status => true, :reload => true, :restart => true
+      supports status: true, reload: true, restart: true
       action([:stop, :disable])
     end
   end
 end
-
-
