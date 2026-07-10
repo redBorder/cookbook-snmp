73d0248c2db46a29c1d212d2dad26d4c74dc7292
José Jiménez <jjimenez@redborder.com>
2026-07-10 10:58:47 +0100

    Delete whitespace

--- resources/providers/config.rb
+++ resources/providers/config.rb
@@ -28,7 +28,7 @@ action :add do
     trap_sensors = new_resource.trap_sensors.map do |s|
       { 'name' => (s['rbname'].nil? ? s.name : s['rbname']), 'ip' => s['ipaddress'] }
     end
-    
+
     trap_sensors = trap_sensors.reject { |s| s['ip'].nil? || s['ip'].to_s.empty? }
 
     dnf_package 'net-snmp' do
