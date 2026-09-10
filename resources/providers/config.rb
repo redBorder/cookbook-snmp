4531b0304b2a9d07d5ee5c4bf51e6b15232943d8
José Jiménez <jjimenez@redborder.com>
2026-09-10 16:11:57 +0100

    Add new config for trap v3

--- resources/providers/config.rb
+++ resources/providers/config.rb
@@ -25,11 +25,33 @@ action :add do
     snmp_pass = new_resource.snmp_pass
     config_dir = new_resource.config_dir
 
+    v3_level_keyword = { 'noAuthNoPriv' => 'noauth', 'authNoPriv' => 'auth', 'authPriv' => 'priv' }
+
     trap_sensors = new_resource.trap_sensors.map do |s|
-      { 'name' => (s['rbname'].nil? ? s.name : s['rbname']), 'ip' => s['ipaddress'] }
+      redborder = s['redborder'] || {}
+      { 'name' => (s['rbname'].nil? ? s.name : s['rbname']),
+        'ip' => s['ipaddress'],
+        'snmp_version' => redborder['trap_snmp_version'],
+        'community' => redborder['trap_auth_community'],
+        'v3_user' => redborder['trap_v3_user'],
+        'v3_engine_id' => redborder['trap_v3_engine_id'],
+        'v3_auth_protocol' => redborder['trap_v3_auth_protocol'],
+        'v3_auth_password' => redborder['trap_v3_auth_password'],
+        'v3_priv_protocol' => redborder['trap_v3_priv_protocol'],
+        'v3_priv_password' => redborder['trap_v3_priv_password'],
+        'v3_level_keyword' => v3_level_keyword[redborder['trap_v3_security_level']] }
     end
 
-    trap_sensors = trap_sensors.reject { |s| s['ip'].nil? || s['ip'].to_s.empty? }
+    trap_sensors = trap_sensors.reject do |s|
+      next true if s['ip'].nil? || s['ip'].to_s.empty?
+
+      if s['snmp_version'] == '3'
+        s['v3_user'].nil? || s['v3_user'].to_s.empty? ||
+        s['v3_engine_id'].nil? || s['v3_engine_id'].to_s.empty?
+      else
+        s['community'].nil? || s['community'].to_s.empty?
+      end
+    end
 
     dnf_package 'net-snmp' do
       action :upgrade
